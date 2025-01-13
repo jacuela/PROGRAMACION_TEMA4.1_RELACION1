fc12bf9fc7d411fa8c5da05ac76b2cf84427840a
jacuela <jacuela@gmail.com>
2025-01-13 18:36:38 +0100

    lunes

--- src/ejercicio03/Libro.java
+++ src/ejercicio03/Libro.java
@@ -18,7 +18,14 @@ public class Libro {
     //Primer constructor
     public Libro(String isbn, String tit, String aut, int year){
         
-        this.isbn = isbn;
+        if (this.checkISBN(isbn)){
+           isbn=isbn.toUpperCase(); //lo paso a mayusculas
+           this.isbn = isbn;
+        }
+        else{
+           System.out.println("ERROR: isbn incorrecto"); 
+        }
+        
         this.titulo=tit;
         this.autor=aut;
         this.año=year;
@@ -43,6 +50,15 @@ public class Libro {
     }
 
     public void setIsbn(String isbn) {
+        
+        if (this.checkISBN(isbn)){
+           isbn=isbn.toUpperCase(); //lo paso a mayusculas
+           this.isbn = isbn;
+        }
+        else{
+           System.out.println("ERROR: isbn incorrecto"); 
+        }
+        
         this.isbn = isbn;
     }
 
@@ -78,14 +94,43 @@ public class Libro {
         
     }
     
-    private boolean checkISBN(){
+    
+    private boolean checkISBN(String isbn){
+        isbn=isbn.toUpperCase(); //lo paso a mayusculas
         
+        boolean isbnCorrecto=true;
+        
+        //Comprobacion1: Compruebo que el ISBN tiene formato correcto
+        if (isbn.length()!=4) isbnCorrecto=false;
+        else{
+            //Comprobacion2: que el primer caracter sea una letra entre 
+            //la A y la Z
+
+            char letra=isbn.charAt(0);
+            if (letra <'A' || letra>'Z') isbnCorrecto=false;
+
+            //Comprobacion3,4,5: que los siguientes caracteres sean
+            //numeros
+            char digito1=isbn.charAt(1);
+            if (digito1 <'0' || digito1>'9') isbnCorrecto=false;
+
+            char digito2=isbn.charAt(2);
+            if (digito2 <'0' || digito2>'9') isbnCorrecto=false;
+
+            char digito3=isbn.charAt(3);
+            if (digito3 <'0' || digito3>'9') isbnCorrecto=false;
+        }
+        //Compruebo el booleano y devuelvo true o false
+        if (isbnCorrecto==false){
+            return false;
+        }
+        else{
+           return true;
+        }
+        
+        //OPCIONALMENRE -->   return isbnCorrecto;
         
     }
-    
-    
-    
-    
     
     
     
